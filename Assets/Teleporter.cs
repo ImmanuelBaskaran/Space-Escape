8824669abecefce16c724c6b9acab257f630f68d
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2019-10-24 15:10:34 +0100

    Initial Commit
    What else is there to say?

---
+++ Assets/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject from;
+    public GameObject to;
+
+    void OnTriggerEnter()
+    {
+        from.SetActive(false);
+        to.SetActive(true);
+    }
+}
